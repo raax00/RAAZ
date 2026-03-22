dc7d24d804f2eaef97b2324dee8a7fb240c726ee
raax00 <raaxbhaii@gmail.com>
2026-03-22 21:16:06 +0530

    Create discover.dart

---
+++ discover.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RelaxPage extends StatelessWidget {
+  const DiscoverPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text('Relax Page (Backend Ready)', style: TextStyle(fontSize: 24))));
+  }
+}
