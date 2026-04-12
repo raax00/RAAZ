7158af45da8cc2f27588c7316ac02fed6be5310c
raax00 <raaxbhaii@gmail.com>
2026-04-12 14:13:03 +0530

    Create utils/theme_notifier.dart

---
+++ lib/utils/theme_notifier.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ThemeNotifier extends ChangeNotifier {
+  static const String _themeKey = 'app_theme_mode';
+  ThemeMode _themeMode = ThemeMode.light;
+
+  ThemeMode get themeMode => _themeMode;
+
+  ThemeNotifier() {
+    _loadTheme();
+  }
+
+  Future<void> _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String? themeString = prefs.getString(_themeKey);
+    if (themeString != null) {
+      _themeMode = themeString == 'dark' ? ThemeMode.dark : ThemeMode.light;
+    } else {
+      _themeMode = ThemeMode.light;
+    }
+    notifyListeners();
+  }
+
+  Future<void> setTheme(ThemeMode mode) async {
+    _themeMode = mode;
+    notifyListeners();
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_themeKey, mode == ThemeMode.dark ? 'dark' : 'light');
+  }
+
+  void toggleTheme() {
+    setTheme(_themeMode == ThemeMode.dark ? ThemeMode.light : ThemeMode.dark);
+  }
+}
